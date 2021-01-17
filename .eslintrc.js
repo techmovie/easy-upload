e5b26dca26df22a55b0eed4fe09a62ef11d57bfa
techmovie <v.abed@icloud.com>
2021-01-18 01:43:17 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true,
+    greasemonkey: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    $j: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    'no-use-before-define': 0
+  }
+}
