67585916aca2fad36355b5d64eedef19037dd4d9
YK.Yau <556092@qq.com>
2021-02-01 16:28:43 +0800

    Add general function to handle nexusphp site type

--- src/source/index.js
+++ src/source/index.js
@@ -1,35 +1,13 @@
 
-import { CURRENT_SITE_NAME, CURRENT_SITE_INFO} from '../const';
+import { CURRENT_SITE_NAME, CURRENT_SITE_INFO } from '../const';
 
 import getPTPInfo from './ptp';
-import getCHDInfo from './chd'
-import getMTeamInfo from './mteam'
-import getTTGInfo from './ttg'
-import getOurbitsInfo from './ourbits'
-import getHDSkyInfo from './hdsky'
-
+import getNexusPHPInfo from './nexusphp';
 
 let getTorrentInfo = getPTPInfo;
 
-switch(CURRENT_SITE_NAME) {
-	case 'PTP':
-		getTorrentInfo = getPTPInfo;
-		break;
-	case 'CHD':
-		getTorrentInfo = getCHDInfo;
-		break;
-	case 'MTeam':
-		getTorrentInfo = getMTeamInfo;
-		break;
-	case 'TTG':
-		getTorrentInfo = getTTGInfo;
-		break;
-	case 'OURBITS':
-		getTorrentInfo = getOurbitsInfo;
-		break;
-	case 'HDSKY':
-		getTorrentInfo = getHDSkyInfo;
-		break;
+if (CURRENT_SITE_INFO.siteType === 'NexusPHP') {
+  getTorrentInfo = getNexusPHPInfo;
 }
 
 export default getTorrentInfo;
