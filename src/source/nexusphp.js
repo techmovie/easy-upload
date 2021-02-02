f5160d9e8c1955fbc8c2e1aa2e511ea4cec5f28e
YK.Yau <556092@qq.com>
2021-02-02 11:50:00 +0800

    Improve nexusphp audioCode and areaCode function, bug fix

--- src/source/nexusphp.js
+++ src/source/nexusphp.js
@@ -5,10 +5,10 @@ import { CURRENT_SITE_INFO, CURRENT_SITE_NAME, TORRENT_INFO } from '../const';
  * TODO: 音频和地区处理
  */
 export default () => {
-  const title = $('#top').prop('firstChild').nodeValue.trim();
+  let title = $('#top').prop('firstChild').nodeValue.trim();
   const year = title.match(/(19|20)\d{2}/g);
   const images = $("td.rowhead:contains('简介'), td.rowhead:contains('簡介')").next().find('img');
-  let metaInfo = $("td.rowhead:contains('基本信息'), td.rowhead:contains('基本資訊')").next().text();
+  let metaInfo = $("td.rowhead:contains('基本信息'), td.rowhead:contains('基本資訊')").next().text().replace(/：/g, ':');
   let subtitle = $("td.rowhead:contains('副标题'), td.rowhead:contains('副標題')").next().text();
   let imdbUrl = $('#kimdb>a').attr('href');
   let description = $('#kdescr').clone().find('fieldset').remove().text().trim();
@@ -29,9 +29,9 @@ export default () => {
     description = doubaninfo ? doubaninfo.replace(/<br>|<br\/>/g, '\n').trim() : '';
   }
 
-  // if (CURRENT_SITE_NAME === 'MTeam') {
-  //   imdbUrl = $('.imdbnew2 a:first').attr('href');
-  // }
+  if (CURRENT_SITE_NAME === 'FRDS') {
+    [title, subtitle] = [subtitle, title];
+  }
   // 站点自定义数据覆盖 结束
 
   const { category, videoType, videoCodes, audioCodes, resolution, processing } = getMetaInfo(metaInfo);
@@ -51,9 +51,9 @@ export default () => {
   TORRENT_INFO.bdinfo = getBDInfo();
   TORRENT_INFO.screenshots = getImages(images);
 
-  TORRENT_INFO.audioCodes = audioCodes;
+  TORRENT_INFO.audioCodes = getAudioCodes(audioCodes);
   // TORRENT_INFO.source = TODO;
-  TORRENT_INFO.area = processing;
+  TORRENT_INFO.area = getAreaCode(processing);
 
   return TORRENT_INFO;
 };
@@ -73,22 +73,18 @@ const getMetaInfo = (metaInfo) => {
   // 馒头的媒介来源在分类中
   if (CURRENT_SITE_NAME === 'MTeam') {
     videoType = category;
-  } else if (metaInfo.match(/媒介|來源/)) {
-    videoType = metaInfo.substr(metaInfo.match(/媒介|來源/).index).split('   ')[0].split(':')[1].trim();
+  } else if (metaInfo.match(/媒介|来源/)) {
+    videoType = metaInfo.substr(metaInfo.match(/媒介|来源/).index).split('   ')[0].split(':')[1].trim();
   }
-
   if (metaInfo.match(/编码|編碼/)) {
     videoCodes = metaInfo.substr(metaInfo.match(/编码|編碼/).index).split('   ')[0].split(':')[1].trim();
   }
-
   if (metaInfo.match(/音频/)) {
     audioCodes = metaInfo.substr(metaInfo.match(/音频/).index).split('   ')[0].split(':')[1].trim();
   }
-
   if (metaInfo.match(/分辨率|格式|解析度/)) {
     resolution = metaInfo.substr(metaInfo.match(/分辨率|格式|解析度/).index).split('   ')[0].split(':')[1].trim();
   }
-
   if (metaInfo.match(/处理|處理|地区/)) {
     processing = metaInfo.substr(metaInfo.match(/处理|處理|地区/).index).split('   ')[0].split(':')[1].trim();
   }
@@ -146,10 +142,16 @@ const getCategory = (videoType) => {
     return 'movie';
   } else if (videoType.match(/tv|drama|剧集/ig)) {
     return 'tv';
-  } else if (videoType.match(/document|纪录/ig)) {
+  } else if (videoType.match(/综艺/ig)) {
+    return 'variety';
+  } else if (videoType.match(/document|纪录|紀錄/ig)) {
     return 'documentory';
   } else if (videoType.match(/sport|体育/ig)) {
     return 'sport';
+  } else if (videoType.match(/mv|演唱/ig)) {
+    return 'concert';
+  } else if (videoType.match(/anime|动|画|漫/ig)) {
+    return 'cartoon';
   }
   return '';
 };
@@ -183,11 +185,26 @@ const getVideoCodes = (codes) => {
   if (codes === '') {
     return '';
   }
-  codes = codes.replace(/[.-]/g, '').toLowerCase();
+  codes = codes.replace(/[.-]|[ ]/g, '').toLowerCase();
   if (codes.match(/265|hevc/ig)) {
     return 'x265';
   } else if (codes.match(/264|avc/ig)) {
     return 'x264';
+  }
+  return codes;
+};
+
+/**
+ * 格式化音频编码格式
+ * @param {code} codes 编码文字
+ */
+const getAudioCodes = (codes) => {
+  if (codes === '') {
+    return '';
+  }
+  codes = codes.replace(/[.-]|[ ]/g, '').toLowerCase();
+  if (codes.match(/dtshd/ig)) {
+    return 'dtshdma';
   }
   return codes;
 };
@@ -221,33 +238,21 @@ const getResolution = (resolution) => {
   return resolution;
 };
 
-// const getAreaCode = () => {
-//   const europeList = ['Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Kazakhstan', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom', 'Vatican City'];
-//   let country = [];
-//   const matchArray = $('#movieinfo div').text().match(/Country:\s+([^\n]+)/);
-//   if (matchArray && matchArray.length > 0) {
-//     country = matchArray[1].replace(/(,)\s+/g, '$1').split(',');
-//   }
-//   if (country[0]) {
-//     if (country[0].match(/USA|Canada/i)) {
-//       return 'US';
-//     } else if (europeList.includes(country[0])) {
-//       return 'EU';
-//     } else if (country[0].match(/Japan/i)) {
-//       return 'JP';
-//     } else if (country[0].match(/Korea/i)) {
-//       return 'KR';
-//     } else if (country[0].match(/Taiwan/i)) {
-//       return 'TW';
-//     } else if (country[0].match(/Hong Kong/i)) {
-//       return 'HK';
-//     } else if (country[0].match(/China/i)) {
-//       return 'CN';
-//     }
-//   }
-//   return 'OT';
-// };
-
-// export {
-//   getDefaultData, getVideoType, getCategory, getMeta, getImages, getVideoCodes, getBDInfo, getResolution,
-// };
+const getAreaCode = (area) => {
+  if (area.match(/CN|大陆|中|内地|Mainland/i)) {
+    return 'CN';
+  } else if (area.match(/HK|港|HongKong/i)) {
+    return 'HK';
+  } else if (area.match(/TW|台|Taiwan/i)) {
+    return 'TW';
+  } else if (area.match(/JP|日|Japan/i)) {
+    return 'JP';
+  } else if (area.match(/KR|韩|Korean/i)) {
+    return 'KR';
+  } else if (area.match(/US|美/i)) {
+    return 'US';
+  } else if (area.match(/欧|英|法|EU/i)) {
+    return 'EU';
+  }
+  return 'OT';
+};
