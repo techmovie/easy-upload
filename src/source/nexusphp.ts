1a3fbd84cf16c6a612c4523814bf20133dd1e6c3
ted423 <priston910423@gmail.com>
2022-06-15 16:56:19 +0800

    style: fix style

--- src/source/nexusphp.ts
+++ src/source/nexusphp.ts
@@ -71,9 +71,10 @@ export default async () => {
     const element = document.createElement('div');
     $(element).html($('#outer td').has('#kdescr').html());
     descriptionBBCode = getFilterBBCode(element);
-    const mediainfo = $("div.codemain > pre:contains('Unique ID')")
-    if(mediainfo[0]) 
-    TORRENT_INFO.mediaInfo = mediainfo.text()
+    const mediainfo = $("div.codemain > pre:contains('Unique ID')");
+    if(mediainfo[0]) {
+      TORRENT_INFO.mediaInfo = mediainfo.text()
+    }
     descriptionBBCode = descriptionBBCode.replace(/\[quote\]GeneralVideo[^\[]*\[\/quote\]/, '');
   }
 
