ff85500f4fd22bf987aca09496d3268aa33678a4
ted423 <priston910423@gmail.com>
2022-06-15 21:55:31 +0800

    Revert "fix(frds): no douban info"
    This reverts commit 3bf556ecc4ac9cbaafab4c17542b3c748f636b6b.

--- src/target/index.ts
+++ src/target/index.ts
@@ -126,7 +126,7 @@ const fillTargetForm = (info:TorrentInfo.Info) => {
   if (info.description) {
     // 去简介前的空格和换行
     description = info.description.replace(/^(\s+)/g, '');
-    if (isChineseTacker(currentSiteInfo.siteType) && CURRENT_SITE_NAME !== 'SSD' && CURRENT_SITE_NAME !== 'KEEPFRDS') {
+    if (isChineseTacker(currentSiteInfo.siteType) && CURRENT_SITE_NAME !== 'SSD') {
       // 需要拼接豆瓣信息的内站
       const { doubanInfo } = info;
       if (doubanInfo) {
