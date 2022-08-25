de7ea759aa4b1bf75432103d5269f4da74b8f155
techmovie <v.abed@icloud.com>
2022-08-25 18:55:04 +0800

    ci: update action

--- scripts/new-release.js
+++ scripts/new-release.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-const { fileURLToPath } = require('node:url');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
 
-module.exports = (core) => {
+export default function getReleaseLog (core) {
   try {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const changeLogPath = path.join(__dirname, '..', 'CHANGELOG.md');
@@ -14,4 +14,4 @@ module.exports = (core) => {
   } catch (error) {
     core.setFailed(error.message);
   }
-};
+}
