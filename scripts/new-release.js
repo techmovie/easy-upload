6ab6d5a5307737f054bf7f15e87d250d56b9186f
techmovie <v.abed@icloud.com>
2022-08-25 22:46:18 +0800

    ci: add release action

--- scripts/new-release.js
+++ scripts/new-release.js
@@ -2,16 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'node:url';
 
-export default (core) => {
+export default function getReleaseLog (core) {
   try {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const changeLogPath = path.join(__dirname, '..', 'CHANGELOG.md');
     const changeLogData = fs.readFileSync(changeLogPath, 'UTF-8');
     const recentLog = changeLogData.match(/(#{1,}\s\[\d\.\d\.\d{1,}\](.|\n)+?)##\s\[\d\.\d\.\d{1,}\]/)[1];
-    const newVersion = recentLog.match(/\[\d\.\d\.\d{1,}\]/)[0];
+    const { version } = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`));
     core.exportVariable('body', recentLog);
-    core.exportVariable('version', newVersion);
+    core.exportVariable('version', version);
   } catch (error) {
     core.setFailed(error.message);
   }
-};
+}
