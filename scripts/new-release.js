feca3b9515bf654611996cd8aa1f0e5168a8b69a
techmovie <v.abed@icloud.com>
2022-08-25 22:29:15 +0800

    ci: update action

--- scripts/new-release.js
+++ scripts/new-release.js
@@ -8,10 +8,9 @@ export default function getReleaseLog (core) {
     const changeLogPath = path.join(__dirname, '..', 'CHANGELOG.md');
     const changeLogData = fs.readFileSync(changeLogPath, 'UTF-8');
     const recentLog = changeLogData.match(/(#{1,}\s\[\d\.\d\.\d{1,}\](.|\n)+?)##\s\[\d\.\d\.\d{1,}\]/)[1];
-    const newVersion = recentLog.match(/\[\d\.\d\.\d{1,}\]/)[0];
+    const { version } = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`));
     core.exportVariable('body', recentLog);
-    core.exportVariable('version', newVersion);
-    console.log(newVersion);
+    core.exportVariable('version', version);
   } catch (error) {
     core.setFailed(error.message);
   }
