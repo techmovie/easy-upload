2d7146c54d8f15542973ce2461f599692990c9cf
ted423 <priston910423@gmail.com>
2023-02-05 15:07:36 +0800

    feat(zhuque): some fix
    准备使用两个 MutationObserver

--- src/target/zhuque.ts
+++ src/target/zhuque.ts
@@ -8,22 +8,23 @@ const currentSiteInfo = PT_SITE.ZHUQUE;
 
 export default (info: TorrentInfo.Info) => {
   const targetNode = document;
-
-  // Options for the observer (which mutations to observe)
-  const observer = new MutationObserver(mutationRecords => {
-    console.log(mutationRecords); // console.log(the changes)
-    debugger;
-    const i_evt = new Event('input');
+  const insert = new MutationObserver(mutationRecords => {
+    $('input.ant-select-selection-search-input[id]').each(function () { this.dispatchEvent(new Event('keydown')); });
     $(currentSiteInfo.name.selector).val(info.title);
-    $(currentSiteInfo.name.selector)[0].dispatchEvent(i_evt);
-    $(currentSiteInfo.subtitle.selector).val(info.subtitle)
-    $(currentSiteInfo.subtitle.selector)[0].dispatchEvent(i_evt);
-    $(currentSiteInfo.douban.selector).val(info.doubanUrl.match(/subject\/(\d+)/)[1]);
-    $(currentSiteInfo.douban.selector)[0].dispatchEvent(i_evt);
-    /* $("div.ant-select-item-option-content:contains('Movie')").click();// 默认先填Movie
+    $(currentSiteInfo.name.selector)[0].dispatchEvent(new Event('input'));
+    if (info.subtitle) {
+      $(currentSiteInfo.subtitle.selector).val(info.subtitle);
+      $(currentSiteInfo.subtitle.selector)[0].dispatchEvent(new Event('input'));
+    }
+    if (info.doubanUrl) {
+      $(currentSiteInfo.douban.selector).val(info.doubanUrl.match(/subject\/(\d+)/)[1]);
+      $(currentSiteInfo.douban.selector)[0].dispatchEvent(new Event('input'));
+    }
+
+    $("div.ant-select-item-option-content:contains('Movie')").click();// 默认先填Movie
     if (info.videoType === 'encode') {
       $("div.ant-select-item-option-content:contains('Encode')").click();
-    } */
+    }
     let screenshotStr = '';
     if (info.screenshots.length > 0) {
       info.screenshots.forEach(img => {
@@ -41,21 +42,18 @@ export default (info: TorrentInfo.Info) => {
         }
       });
     }
-    observer.disconnect();
+    const selectNodeParent = document.querySelector('form');
+    const select = new MutationObserver(mutationRecords => { console.log(mutationRecords); });
+    select.observe(selectNodeParent, { attributes: false, childList: true, subtree: true, characterDataOldValue: false });
+    insert.disconnect();
   });
+  insert.observe(targetNode, { attributes: false, childList: false, subtree: true, characterDataOldValue: false });
 
-  // 观察除了特性之外的所有变动
-  observer.observe(targetNode, {
-    childList: false, // 观察直接子节点
-    subtree: true, // 及其更低的后代节点
-    characterDataOldValue: false, // 将旧的数据传递给回调
-  });
 };
 function fillMediaInfo (info: TorrentInfo.Info) {
   $(currentSiteInfo.mediaInfo.selector).val(info.mediaInfo);
   $(currentSiteInfo.mediaInfo.selector)[0].dispatchEvent(new Event('input'));
 }
-
 function fillDescription (info: TorrentInfo.Info) {
   let description = info.description.replace(`[quote]${info.mediaInfo}[/quote]`, '');
   description = description.replaceAll(/\[url.*\[\/url\]/g, '').replaceAll(/\[img.*\[\/img\]/g, '').replaceAll(/\[\/?(i|b|center|quote|size|color)\]/g, '').replaceAll(/\[(size|color)\=#?[a-zA-Z0-9]*\]/g, '').replaceAll(/\n\n*/g, '\n');
