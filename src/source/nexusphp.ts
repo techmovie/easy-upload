c192f30654c8cf107946945182335255f924c49c
ted423 <priston910423@gmail.com>
2023-02-13 22:17:09 +0800

    feat(frds): get comparisons

--- src/source/nexusphp.ts
+++ src/source/nexusphp.ts
@@ -80,6 +80,27 @@ export default async () => {
       });
     }
     descriptionBBCode = descriptionBBCode.replace(' 截图对比(点击空白处展开)', '截图对比');
+    interface ComparisonObj{
+      comparisonstitle: string
+      imgs: string[]
+      reason: string
+    }
+    const comparisonArray = $('fieldset[onclick]').toArray() || [];
+    const comparisons:ComparisonObj[] = [];
+    comparisonArray.forEach(item => {
+      const imgs: string[] = [];
+      $(item).find('a').toArray().forEach(img => {
+        if (img.href) imgs.push(img.href);
+      });
+      const comparisonstitle = $(item).find('legend').text().replace(' 截图对比(点击空白处展开):', '').trim();
+      const reason: string = '';
+      comparisons.push({
+        comparisonstitle,
+        imgs,
+        reason,
+      });
+    });
+    TORRENT_INFO.comparisons = comparisons;
     if (!descriptionBBCode.match('豆瓣评分')) {
       const imdbRate = $('#kimdb span.imdbwp__rating').text().replace('\nRating: ', '');
       const doubanInfo = $('#kdouban .imdbwp__content').text().replaceAll(/\n{2,}/g, '\n').replace(/\n[0-9]?[0-9]\.[0-9]\n/g, '\n').replaceAll('\n', '\n◎').replace(/\n◎$/, '\n').replace('◎Rating:', `◎IMDb链接:${siteImdbUrl}\n◎IMDb评分: ${imdbRate}\n◎豆瓣链接: ${TORRENT_INFO.doubanUrl}\n◎豆瓣评分:`);
